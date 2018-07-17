719f039eba250a967b6ca5e8f5b109bff447b03c
hellomoto <hellomoto@p1.zcash.ph>
2018-07-17 11:26:10 +0800

    typo

--- src/pow.cpp
+++ src/pow.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2014 The Commercium developers
+// Copyright (c) 2009-2015 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -14,53 +14,52 @@
 #include "uint256.h"
 #include "util.h"
 #include "utilstrencodings.h"
-
 #include <algorithm>
+#include <math.h>
 
-unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params)
+unsigned int GetNextWorkRequired(const CBlockIndex *pindexLast, const CBlockHeader *pblock, const Consensus::Params &params)
 {
-	unsigned int pow = UintToArith256(params.powLimit).GetCompact();
-	int nHeight = pindexLast->nHeight + 1;
+        unsigned int pow = UintToArith256(params.powLimit).GetCompact();
+        int nHeight = pindexLast->nHeight + 1;
 
-	const CBlockIndex *pindexFirst = pindexLast;
-	arith_uint256 bnTot {0};
-	for (int i = 0; pindexFirst && i < params.nPowAveragingWindow; ++i)
-	{
-		arith_uint256 bnTmp;
-		bnTmp.SetCompact(pindexFirst->nBits);
-		bnTot += bnTmp;
-		pindexFirst = pindexFirst->pprev;
-	}
+        const CBlockIndex *pindexFirst = pindexLast;
+        arith_uint256 bnTot {0};
+        for (int i = 0; pindexFirst && i < params.nPowAveragingWindow; ++i)
+        {
+                arith_uint256 bnTmp;
+                bnTmp.SetCompact(pindexFirst->nBits);
+                bnTot += bnTmp;
+                pindexFirst = pindexFirst->pprev;
+        }
 
-	if (pindexFirst == NULL)
-	{
-		return pow;
-	}
+        if (pindexFirst == NULL)
+        {
+                return pow;
+        }
 
-	arith_uint256 bnAvg{bnTot / params.nPowAveragingWindow};
+        arith_uint256 bnAvg{bnTot / params.nPowAveragingWindow};
 
-	return CalculateNextWorkRequired(bnAvg, pindexLast->GetMedianTimePast(), pindexFirst->GetMedianTimePast(), params);
+        return CalculateNextWorkRequired(bnAvg, pindexLast->GetMedianTimePast(), pindexFirst->GetMedianTimePast(), params);
 }
 
-unsigned int CalculateNextWorkRequired(arith_uint256 bnAvg, int64_t lastBlockTime, int64_t firstBlockTime, const Consensus::Params& params)
+unsigned int CalculateNextWorkRequired(arith_uint256 bnAvg, int64_t lastBlockTime, int64_t firstBlockTime, const Consensus::Params &params)
 {
-	// limit adjustment
-	int64_t actualTimeSpan = lastBlockTime - firstBlockTime;
+        // limit adjustment
+        int64_t actualTimeSpan = lastBlockTime - firstBlockTime;
 
-	actualTimeSpan = std::max(actualTimeSpan, params.MinActualTimespan());
-	actualTimeSpan = std::min(actualTimeSpan, params.MaxActualTimespan());
+        actualTimeSpan = std::max(actualTimeSpan, params.MinActualTimespan());
+        actualTimeSpan = std::min(actualTimeSpan, params.MaxActualTimespan());
 
-	// retarget
-	const arith_uint256 pow = UintToArith256(params.powLimit);
-	arith_uint256 bnNew {bnAvg};
-	bnNew /= params.AveragingWindowTimespan();
-	bnNew *= actualTimeSpan;
+        // retarget
+        const arith_uint256 pow = UintToArith256(params.powLimit);
+        arith_uint256 bnNew {bnAvg};
+        bnNew /= params.AveragingWindowTimespan();
+        bnNew *= actualTimeSpan;
 
-	bnNew = std::min(pow, bnNew);
+        bnNew = std::min(pow, bnNew);
 
-	return bnNew.GetCompact();
+        return bnNew.GetCompact();
 }
-
 
 bool CheckEquihashSolution(const CBlockHeader *pblock, const CChainParams& params)
 {
@@ -92,9 +91,9 @@ bool CheckEquihashSolution(const CBlockHeader *pblock, const CChainParams& param
     bool isValid;
     EhIsValidSolution(n, k, state, pblock->nSolution, isValid);
     if (!isValid)
-	{
+        {
         return error("CheckEquihashSolution(): invalid solution");
-	}
+        }
     return true;
 }
 
@@ -108,11 +107,11 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&
 
     // Check range
     if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(params.powLimit))
-        return error("CheckProofOfWork(): nBits below minimum work");
+        return false;
 
     // Check proof of work matches claimed amount
     if (UintToArith256(hash) > bnTarget)
-        return error("CheckProofOfWork(): hash doesn't match nBits");
+        return false;
 
     return true;
 }
